56cd0e3f66858c7b121944f488921fbfd698692f
心洗 <758251611@qq.com>
2022-04-29 17:42:17 +0800

    Solution3Finish

--- Leaper/Solution3.cpp
+++ Leaper/Solution3.cpp
@@ -11,8 +11,23 @@ using std::list;
 using std::vector;
 using std::unordered_map;
 
+struct Info;
+
 typedef list<int> List;
-typedef unordered_map<int, std::pair<int, int>> Dict;
+typedef unordered_map<int, Info> Dict;
+
+enum Status
+{
+	_NOT_USED,
+	_CREATING,
+	_CREATED
+};
+
+struct Info {
+	int first{};
+	int second{};
+	int status{};
+};
 
 struct Node
 {
@@ -44,20 +59,33 @@ void Show(list<int>& list) {
 	std::cout << std::endl;
 }
 
+class contradiction {
+public:
+	contradiction(int& code):code(code) {
+
+	}
+	const int _getcode() const {
+		return code;
+	}
+private:
+	int code;
+};
+
 int main()
 {
 	/* 例1 */
 	Nodes nodes1;
-	//nodes1.AddNode(Node(1, -1, -1));
-	//nodes1.AddNode(Node(2, 1, -1));
-	//nodes1.AddNode(Node(3, 2, 1));
-	//nodes1.AddNode(Node(4, 2, 5));
-	//nodes1.AddNode(Node(5, 2, 1));
+
+	nodes1.AddNode(Node(6, 7, -1));
+	nodes1.AddNode(Node(7, 6, -1));
+	nodes1.AddNode(Node(1, -1, -1));
+	nodes1.AddNode(Node(2, 1, -1));
+	nodes1.AddNode(Node(3, 2, 1));
+	nodes1.AddNode(Node(4, 2, 5));
+	nodes1.AddNode(Node(5, 2, 1));
 
 	//下面这两行的内容构成矛盾,执行Solution时导致整个程序错误返回
 	//原因可能是爆栈,代码需要具备排查矛盾的功能
-	nodes1.AddNode(Node(6, 7, -1));
-	nodes1.AddNode(Node(7, 6, -1));
 	list<int> listId1 = nodes1.Solution(); 
 	Show(listId1);
 
@@ -81,8 +109,15 @@ int main()
 void AddList(List& list, Dict& dict, int code) {
 	
 	//若该工序已登记
-	if (find(list.begin(), list.end(), code) != list.end())
+	if (dict[code].status == _CREATED)
 		return;
+
+	if (dict[code].status == _CREATING) {
+		throw contradiction(code);
+		return;
+	}
+
+	dict[code].status = _CREATING;
 
 	bool isFirstExists{ dict.find(dict[code].first) != dict.end() };
 	bool isSecondExists{ dict.find(dict[code].second) != dict.end() };
@@ -90,24 +125,33 @@ void AddList(List& list, Dict& dict, int code) {
 	//若该工序不存在需求,直接加入list
 	if (isFirstExists == false && isSecondExists == false) {
 		list.push_front(code);
+		dict[code].status = _CREATED;
 		return;
 	}
 
-	//若需求first存在,登记该需求
-	if (isFirstExists) {
-		if (find(list.begin(), list.end(), dict[code].first) == list.end()) {
-			AddList(list, dict, dict[code].first);
+	try {
+		//若需求first存在,登记该需求
+		if (isFirstExists) {
+			if (find(list.begin(), list.end(), dict[code].first) == list.end()) {
+				AddList(list, dict, dict[code].first);
+			}
+		}
+
+		//若需求second存在,登记该需求
+		if (isSecondExists) {
+			if (find(list.begin(), list.end(), dict[code].second) == list.end()) {
+				AddList(list, dict, dict[code].second);
+			}
 		}
 	}
-
-	//若需求second存在,登记该需求
-	if (isSecondExists) {
-		if (find(list.begin(), list.end(), dict[code].second) == list.end()) {
-			AddList(list, dict, dict[code].second);
-		}
+	catch (contradiction& e) {
+		std::cout << std::endl << std::endl << "\tSolution执行异常:" << std::endl << std::endl;
+		std::cout << "\t\t工序 " << code << " - " << "工序 "<< e._getcode() << " 前置工序冲突" << std::endl << std::endl;
+		exit(-1);
 	}
 
 	list.push_back(code);
+	dict[code].status = _CREATED;
 
 	return;
 }
@@ -123,7 +167,7 @@ list<int> Nodes::Solution()
 
 	//将工序编号及需求加入字典
 	for (const auto& node : this->mNodes)
-		dict.emplace(node.nId, std::make_pair(node.nPre1, node.nPre2));
+		dict.emplace(node.nId, Info{ node.nPre1, node.nPre2, _NOT_USED });
 
 	//构造链表
 	for(const auto& node : this->mNodes)
